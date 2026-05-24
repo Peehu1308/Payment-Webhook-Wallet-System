a0e44f8c5aed4fc02450d38be71ddc9249b204fe
Peehu1308 <peehumishra1308@gmail.com>
2026-05-24 23:16:39 +0530

    Update schema.sql

--- Backend/sql/schema.sql
+++ Backend/sql/schema.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE users(
 id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
 name VARCHAR(100),
@@ -23,7 +25,7 @@ created_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE webhooks(
-id UUID PRIMARY KEY DEFAULT gen_random-uuid(),
+id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 webhook_id VARCHAR(255) UNIQUE NOT NULL,
 payload JSONB,
 processed_at TIMESTAMP DEFAULT NOW()
